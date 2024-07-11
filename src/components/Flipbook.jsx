a7b58635514fd817229ce265a92f7f683e6c5263
AlexOT03 <alexot855@gmail.com>
2024-07-11 13:40:46 -0600

    fix flipbook works

--- src/components/Flipbook.jsx
+++ src/components/Flipbook.jsx
@@ -1,21 +1,40 @@
+import React from 'react';
 import HTMLFlipBook from 'react-pageflip';
 
-const Flipbook = () => {
+const Page = React.forwardRef((props, ref) => {
     return (
-        <HTMLFlipBook width={300} height={500}>
-            <div className="demoPage bg-gray-400">Page 1</div>
-            <div className="demoPage bg-gray-400">Page 2</div>
-            <div className="demoPage bg-gray-400">Page 3</div>
-            <div className="demoPage bg-gray-400">Page 4</div>
-            <div className="demoPage bg-gray-400">Page 5</div>
-            <div className="demoPage bg-gray-400">Page 6</div>
-            <div className="demoPage bg-gray-400">Page 7</div>
-            <div className="demoPage bg-gray-400">Page 8</div>
-            <div className="demoPage bg-gray-400">Page 9</div>
-            <div className="demoPage bg-gray-400">Page 10</div>
-            <div className="demoPage bg-gray-400">Page 11</div>
+        <div className="demoPage" ref={ref}>
+            {props.children}
+        </div>
+    );
+});
+
+function Flipbook(props) {
+    const imagePaths = [
+        props.path + '0001.jpg',
+        props.path + '0002.jpg',
+        props.path + '0003.jpg',
+        props.path + '0004.jpg',
+        props.path + '0005.jpg',
+        props.path + '0006.jpg',
+        props.path + '0007.jpg',
+        props.path + '0008.jpg',
+        props.path + '0009.jpg',
+        props.path + '0010.jpg',
+        props.path + '0011.jpg',
+    ];
+
+    const pages = imagePaths.map((path, index) => (
+        <Page key={index} number={index + 1}>
+            <img className='h-full' src={path} alt={`Page ${index + 1}`} />
+        </Page>
+    ));
+
+    return (
+        <HTMLFlipBook width={385} height={500} showCover={true} mobileScrollSupport={false}>
+            {pages}
         </HTMLFlipBook>
-    )
-};
+    );
+}
 
 export default Flipbook;
