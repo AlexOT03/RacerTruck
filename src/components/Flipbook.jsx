7bff5ce8b5e25c887f28daee31f1692c0f5e8c5d
AlexOT03 <alexot855@gmail.com>
2024-07-15 12:20:43 -0600

    fix flipbook work

--- src/components/Flipbook.jsx
+++ src/components/Flipbook.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from 'react'
 import HTMLFlipBook from 'react-pageflip';
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import pdf from '/pdf/all-conditional.pdf';
 import { useTranslations } from '../i18n/utils';
 
 var lang = window.location.pathname.split('/')[1];
 if (!lang || lang !== 'es') lang = 'en';
 const t = useTranslations(lang);
 
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+  ).toString();
 
 const PageCover = React.forwardRef((props, ref) => {
     return (
@@ -17,46 +24,39 @@ const PageCover = React.forwardRef((props, ref) => {
     );
 });
 
-const Page = React.forwardRef((props, ref) => {
+const Pages = React.forwardRef((props, ref) => {
     return (
-        <div className="page bg-white" ref={ref} >
-            <div className='page-content flex justify-center items-center'>
-                {props.children}
-            </div>
+        <div className="page" ref={ref} >
+            <p>{props.children}</p>
         </div>
     );
 });
 
+Pages.displayName = 'Pages';
+
 function Flipbook() {
 
-    const imagePaths = [
-        '/magazines/project-pdf/test_page-0001.jpg',
-        '/magazines/project-pdf/test_page-0002.jpg',
-        '/magazines/project-pdf/test_page-0003.jpg',
-        '/magazines/project-pdf/test_page-0004.jpg',
-        '/magazines/project-pdf/test_page-0005.jpg',
-        '/magazines/project-pdf/test_page-0006.jpg',
-        '/magazines/project-pdf/test_page-0007.jpg',
-        '/magazines/project-pdf/test_page-0008.jpg',
-        '/magazines/project-pdf/test_page-0009.jpg',
-        '/magazines/project-pdf/test_page-0010.jpg',
-        '/magazines/project-pdf/test_page-0011.jpg',
-    ];
+    const [numPages, setNumPages] = useState();
 
-    const pages = imagePaths.map((path, index) => (
-        <Page key={index}>
-            <img src={path} alt={`Page ${index + 1}`} className="object-contain h-full w-full" />
-        </Page>
-    ));
-
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
     return (
         <div className='h-full w-full p-5 flex flex-col justify-center items-center overflow-hidden'>
             <h1 className='text-3xl text-center font-bold pb-5'>{t('flipbook.title')}</h1>
             <HTMLFlipBook width={460} height={600} maxShadowOpacity={0.3} showCover={true}>
-                {pages}
+                {
+                    [...Array(numPages).keys()].map((pNum) => (
+                        <Pages key={pNum} number={pNum + 1}>
+                            <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+                                <Page pageNumber={pNum+1} width={460} renderAnnotationLayer={false} renderTextLayer={false} />
+                            </Document>
+                        </Pages>
+                    ))
+                }
             </HTMLFlipBook>
         </div>
     );
 }
 
-export default Flipbook;
+export default Flipbook
