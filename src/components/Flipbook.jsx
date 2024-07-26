34d32a877badae334b5e23139b254b57a6084f0d
AlexOT03 <alexot855@gmail.com>
2024-07-25 21:16:06 -0600

    fix flipbook position

--- src/components/Flipbook.jsx
+++ src/components/Flipbook.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import HTMLFlipBook from "react-pageflip";
-import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import pdf from "/pdf/Red-Modern-Car-Magazine.pdf";
 import { useTranslations } from "../i18n/utils";
@@ -30,7 +29,7 @@ function FlipBook() {
 
     const [loading, setLoading] = useState(true);
 	const [numPages, setNumPages] = useState(null);
-    const [initialMargin, setInitialMargin] = useState('-50%');
+    const [initialMargin, setInitialMargin] = useState("ml-[-40%]");
 
 	const onDocumentLoadSuccess = ({ numPages }) => {
 		setNumPages(numPages);
@@ -41,10 +40,20 @@ function FlipBook() {
 	};
 
     const handleFirstPageClick = () => {
-		if (initialMargin === '-50%') {
-			setInitialMargin('0');
-		}
-	};
+        if (initialMargin === "ml-[-40%]") {
+            setInitialMargin("ml-[7%]");
+        }
+    };
+
+    const handleFlip = (e) => {
+        if (e.data === 0) {
+            setInitialMargin("ml-[-40%]");
+        } else if (e.data === numPages - 1) {
+            setInitialMargin("ml-[50%]");
+        } else {
+            setInitialMargin("ml-[7%]");
+        }
+    };
 
 	return (
         <section id="magazine" className="h-fit flex flex-col justify-end items-center md:justify-center scroll-mx-2 pb-20 overflow-hidden max-w-screen-xl mx-auto">
@@ -52,24 +61,23 @@ function FlipBook() {
             <div className="text-4xl font-bold p-5">
                 <h1>{pdf_name}</h1>
             </div>
-            <div className="h-full w-full transition-all"
-				style={{ marginLeft: initialMargin }}
+            <div className={`h-full w-full relative transition-all ${initialMargin}`}
 				onClick={handleFirstPageClick}
 			>
-            <HTMLFlipBook width={595} height={769} showCover={true} flippingTime={800} maxShadowOpacity={0.6}>
-                {[...Array(numPages).keys()].map((n) => (
-                    <Pages number={`${n + 1}`}>
-                        <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
-                            <Page
-                                pageNumber={n + 1}
-                                width={595}
-                                renderAnnotationLayer={false} 
-                                renderTextLayer={false}
-                            />
-                        </Document>
-                    </Pages>
-                ))}
-            </HTMLFlipBook>
+                <HTMLFlipBook width={595} height={769} showCover={true} flippingTime={800} maxShadowOpacity={0.6} onFlip={handleFlip}>
+                    {[...Array(numPages).keys()].map((n) => (
+                        <Pages number={`${n + 1}`}>
+                            <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+                                <Page
+                                    pageNumber={n + 1}
+                                    width={595}
+                                    renderAnnotationLayer={false} 
+                                    renderTextLayer={false}
+                                />
+                            </Document>
+                        </Pages>
+                    ))}
+                </HTMLFlipBook>
             </div>
         </section>
 	);
