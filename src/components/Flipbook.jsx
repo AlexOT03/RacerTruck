dfd256d7eb56d951228aec9c7abac63abbe6baa8
AlexOT03 <alexot855@gmail.com>
2024-07-29 15:59:50 -0600

    add flipbook style

--- src/components/Flipbook.jsx
+++ src/components/Flipbook.jsx
@@ -26,6 +26,7 @@ function FlipBook() {
 
     const [loading, setLoading] = useState(true);
 	const [numPages, setNumPages] = useState(null);
+    const [initialMargin, setInitialMargin] = useState("lg:ml-[-40%]");
 
     const onDocumentLoadSuccess = useCallback(({ numPages }) => {
         setNumPages(numPages);
@@ -34,33 +35,46 @@ function FlipBook() {
         }, 1000);
     }, []);
 
+    const handleFlip = (e) => {
+        if (e.data === 0) {
+            setInitialMargin("lg:ml-[-40%]");
+        } else if (e.data === numPages - 1) {
+            setInitialMargin("lg:ml-[45%]");
+        } else {
+            setInitialMargin("lg:ml-[0%]");
+        }
+    };
+
 	return (
-        <section id="magazine" className="h-fit flex flex-col ml- justify-end items-center md:justify-center scroll-mx-2 pb-20 overflow-hidden max-w-screen-xl mx-auto">
+        <section id="magazine" className="h-fit flex flex-col justify-end items-center md:justify-center scroll-mx-2 pb-20 overflow-hidden max-w-screen-xl mx-auto">
             {loading && <LoadingScreen loading={loading} />}
-            <div className="text-4xl font-bold p-5">
+            <div className="text-4xl text-center font-bold">
                 <h1>{pdfName}</h1>
             </div>
-            <HTMLFlipBook 
-                width={550}
-                height={711}
-                showCover={true} 
-                flippingTime={800} 
-                maxShadowOpacity={0.6} 
-                mobileScrollSupport={true} 
-                autoSize={true}>
-                    {[...Array(numPages).keys()].map((n) => (
-                        <Pages key={n}>
-                            <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
-                                <Page
-                                    pageNumber={n + 1}
-                                    width={550}
-                                    renderAnnotationLayer={false} 
-                                    renderTextLayer={false}
-                                />
-                            </Document>
-                        </Pages>
-                    ))}
+            <div className={`h-full w-full m-0 lg:px-20 py-10 transition-all ml-0 ${initialMargin} duration-200 overflow-hidden`}>
+                <HTMLFlipBook 
+                    width={550}
+                    height={711}
+                    showCover={true} 
+                    flippingTime={800} 
+                    maxShadowOpacity={0.6} 
+                    mobileScrollSupport={true} 
+                    autoSize={true}
+                    onFlip={handleFlip}>
+                        {[...Array(numPages).keys()].map((n) => (
+                            <Pages key={n}>
+                                <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+                                    <Page
+                                        pageNumber={n + 1}
+                                        width={550}
+                                        renderAnnotationLayer={false} 
+                                        renderTextLayer={false}
+                                    />
+                                </Document>
+                            </Pages>
+                        ))}
                 </HTMLFlipBook>
+            </div>
         </section>
 	);
 }
