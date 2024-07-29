c03cff7f2ab619bb9b4483deec35aac99fd01e03
AlexOT03 <alexot855@gmail.com>
2024-07-29 08:35:47 -0600

    fix flipbook issues

--- src/components/Flipbook.jsx
+++ src/components/Flipbook.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useCallback, forwardRef, memo } from "react";
 import HTMLFlipBook from "react-pageflip";
 import { Document, Page, pdfjs } from "react-pdf";
 import pdf from "../data/pdf/Cold-Wheels-August-2024.pdf";
@@ -8,40 +8,42 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 	import.meta.url
 ).toString();
 
-let pdf_name = pdf.split("/")[pdf.split("/").length - 1].replace(".pdf", "")
-pdf_name = pdf_name.replace(/[-_]/g, ' ');
 
-const Pages = React.forwardRef((props, ref) => {
-	return (
-		<div className="page" ref={ref}>
-			{props.children}
-		</div>
-	);
-});
+let pdfName = pdf.split("/").pop().replace(".pdf", "").replace(/[-_]/g, ' ');
+
+const Pages = memo(
+    forwardRef((props, ref) => (
+        <div className="page" ref={ref}>
+            {props.children}
+        </div>
+    ))
+);
+
+Pages.displayName = 'Pages';
 
 function FlipBook() {
 
 	const [numPages, setNumPages] = useState(null);
 
-	const onDocumentLoadSuccess = ({ numPages }) => {
-		setNumPages(numPages);
-	};
+    const onDocumentLoadSuccess = useCallback(({ numPages }) => {
+        setNumPages(numPages);
+    }, []);
 
 	return (
         <section id="magazine" className="h-full flex flex-col ml- justify-end items-center md:justify-center scroll-mx-2 pb-20 overflow-hidden max-w-screen-xl mx-auto">
             <div className="text-4xl font-bold p-5">
-                <h1>{pdf_name}</h1>
+                <h1>{pdfName}</h1>
             </div>
             <HTMLFlipBook 
                 width={550}
-                height={733}
+                height={711}
                 showCover={true} 
                 flippingTime={800} 
                 maxShadowOpacity={0.6} 
                 mobileScrollSupport={true} 
                 autoSize={true}>
                     {[...Array(numPages).keys()].map((n) => (
-                        <Pages key={n} number={`${n + 1}`}>
+                        <Pages key={n}>
                             <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
                                 <Page
                                     pageNumber={n + 1}
