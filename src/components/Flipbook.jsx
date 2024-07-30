c3b0c970db7447cdcb82cbf83e46a7485c38343a
AlexOT03 <alexot855@gmail.com>
2024-07-30 09:28:18 -0600

    fix issues on flipbook

--- src/components/Flipbook.jsx
+++ src/components/Flipbook.jsx
@@ -7,7 +7,6 @@ import { useTranslations } from "../i18n/utils";
 
 var lang = window.location.pathname.split("/")[1];
 if (!lang || lang !== "es") lang = "en";
-console.log(lang);
 const t = useTranslations(lang);
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
@@ -93,7 +92,7 @@ function FlipBook() {
                     width={550}
                     height={711}
                     showCover={true} 
-                    flippingTime={800} 
+                    flippingTime={500} 
                     maxShadowOpacity={0.6} 
                     mobileScrollSupport={true} 
                     autoSize={true}
@@ -119,14 +118,14 @@ function FlipBook() {
                 <div className="inline-flex mt-2 xs:mt-0">
                   <button className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 rounded-s hover:bg-gray-900" onClick={prevButtonClick}>
                         <svg className="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
                         </svg>
                         {t('flipbook.prev')}
                   </button>
                   <button className="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900" onClick={nextButtonClick}>
                         {t('flipbook.next')}
                         <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
                   </button>
                 </div>
